700a7761c7f220a4a55dabf5347e6b4eb36af8af
Epiney <epinas@falcon1.hpc.inl.gov>
2018-05-30 13:21:00 -0600

    removed tests that run CashFlow as external Code in RAVEN. Added test that run CashFlow as code directly with python

---
+++ tests/CashFlow_test_NPV_RunAsCode.py
@@ -0,0 +1,19 @@
+
+import os
+import sys
+# run the Cash Flow plugin as stand alone code
+os.system('python ../../../plugins/CashFlow/src/Cash_Flow.py -iXML Cash_Flow_input_NPV.xml -iINP VarInp.txt -o out.out')
+
+# read out.out and compare with gold
+
+with open("out.out") as out:
+  for l in out:
+    pass
+
+gold = float(l)
+if (gold - 627370309.759) < 0.01 :
+  sys.exit(0)
+else:
+  sys.exit(1)
+
+
