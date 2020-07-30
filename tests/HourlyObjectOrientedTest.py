caf97e35bfa4eb94295686ee03eb3e38cb4f1be4
alfoa <andrea.alfonsi@inl.gov>
2020-07-30 15:53:53 -0600

    added headers

--- tests/HourlyObjectOrientedTest.py
+++ tests/HourlyObjectOrientedTest.py
@@ -1,8 +1,24 @@
+# Copyright 2017 Battelle Energy Alliance, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
-  Integration test for using object-oriented API for components.
-  Does not use run-time variables, provides all values through arrays.
-"""
+Created on March 23, 2018
 
+@authors: C. Wang, P. Talbot, A. Alfonsi, A. S. Epiney
+
+Integration test for using object-oriented API for components.
+Does not use run-time variables, provides all values through arrays.
+"""
 import os
 import sys
 import numpy as np
