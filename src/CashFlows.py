fb206db65ad615be5f69f272d4c8beda1e3a7d05
alfoa <andrea.alfonsi@inl.gov>
2020-07-30 15:46:39 -0600

    added header for Opensource

--- src/CashFlows.py
+++ src/CashFlows.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Created on Feb 23, 2017
-
 @authors: A. S. Epiney, P. Talbot, C. Wang, A. Alfonsi
 
 Defines the Economics entity.
