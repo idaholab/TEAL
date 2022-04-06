8442dd990004466af7a70fc03c57d5c1f61f6a31
Han Bao <han.bao@inl.gov>
2022-04-06 12:46:19 -0600

    valid change

--- src/CashFlowPlot.py
+++ src/CashFlowPlot.py
@@ -19,11 +19,16 @@
   Date  :  2021-11-09
 """
 import os
+from matplotlib import cm
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-from PluginBaseClasses.OutStreamPlotPlugin import PlotPlugin, InputTypes, InputData
+try:
+  from ravenframework.PluginBaseClasses.OutStreamPlotPlugin import PlotPlugin, InputTypes, InputData
+except:
+  raise IOError('TEAL ERROR (initialization): RAVEN needs to be installed and TEAL needs to be installed as a plugin to work!')
+
 class CashFlowPlot(PlotPlugin):
   """
     Cash flow plot
@@ -97,41 +102,59 @@ class CashFlowPlot(PlotPlugin):
     if self._startYear is None:
       startYear = 0
     if self._endYear is None:
-      endYear = len(df) - 1
+      endYear = 20 #len(df) - 1
     dfYear = df[startYear:endYear + 1]
-    dfCashFlow = dfYear.loc[:, dfYear.columns.str.endswith("CashFlow") + dfYear.columns.str.endswith("Depreciate") + dfYear.columns.str.endswith("Amortize")]
-    cumulativeCashFlow = dfCashFlow.sum().sort_values(ascending=True) # Sorted cummulative cash flow in Project Year 'endYear' in an ascending order
-    dfCashFlow = dfCashFlow[cumulativeCashFlow.sort_values(ascending=True).index[:len(cumulativeCashFlow)]] # Sorted cash flows
-    yearNetCashFlow = dfCashFlow.sum(axis=1) # Sorted yearly net cash flow
-    cumulativeNetCashFlow = yearNetCashFlow.cumsum() # Cumulative net cash flow
-    labels = pd.DataFrame(dfCashFlow.columns) # Sorted labels
-    # Color setting
-    negativeVariable = len(list(filter(lambda lableLoc: (lableLoc < 0), cumulativeCashFlow))) # count # of outflows
-    positiveVariable = len(list(filter(lambda lableLoc: (lableLoc >= 0), cumulativeCashFlow))) # count # of inflows
+    dfCashFlow = dfYear.loc[:, dfYear.columns.str.contains("_")]
+
+    # sort data by comp name and cum for color setting
+    cumCashFlow = dfCashFlow.sum().sort_values(ascending=True)
+    labelCompAll = [cumCashFlow.index.tolist()[i].partition('_')[0] for i, _ in enumerate(cumCashFlow)]
+    # Comp name: Comp count
+    compCount = pd.Series({i:labelCompAll.count(i) for i in labelCompAll})
+    cumCashFlow = cumCashFlow.set_axis([cumCashFlow.index.tolist(), labelCompAll])
+    # Sorted by Comp names and values
+    cumCashFlow = cumCashFlow.reset_index(level=1).sort_values('level_1').drop('level_1', axis=1).squeeze()
+
+    # Color setting by comp names
     defaultStartColor = 60 # default starting color #. 256 selections in total
     defaultEndColor = 240 # default ending color #. 256 selections in total
-    negativeCmap = plt.get_cmap('Reds')
-    negativeRawCmapList = [negativeCmap(i) for i in range(negativeCmap.N)]
-    negativeCmapList = negativeRawCmapList[defaultEndColor:defaultStartColor:-(defaultEndColor-defaultStartColor)//negativeVariable]
-    positiveCmap = plt.get_cmap('Blues')
-    positiveRawCmapList = [positiveCmap(i) for i in range(positiveCmap.N)]
-    positiveCmapList = positiveRawCmapList[defaultStartColor:defaultEndColor:(defaultEndColor-defaultStartColor)//positiveVariable]
-    cmapList = negativeCmapList + positiveCmapList # Color list
+    colorLib = ['Reds', 'Blues', 'Greens', 'Purples', 'Oranges', 'ocean'] # color library is changable
+    compColor = []
+    for i, _ in enumerate(compCount):
+      _cmap = plt.get_cmap(colorLib[i])
+      _rawCmapList = [_cmap(k) for k in range(_cmap.N)]
+      compColor += _rawCmapList[defaultEndColor:defaultStartColor:-(defaultEndColor-defaultStartColor)//compCount[i]]
+    compColor = pd.DataFrame(compColor).set_axis([cumCashFlow.index.tolist()]).T
+    cumCashFlow = cumCashFlow.sort_values(ascending=True)
+    compColor = compColor[cumCashFlow.index[:len(cumCashFlow)]]
+    compColor = compColor.T.values.tolist()
+    negVar = len(list(filter(lambda lableLoc: (lableLoc < 0), cumCashFlow))) # count # of outflows
+    posVar = len(list(filter(lambda lableLoc: (lableLoc >= 0), cumCashFlow))) # count # of inflows
+
+    dfCashFlow = dfCashFlow[cumCashFlow.index[:len(cumCashFlow)]] # Sorted cash flows
+    yearNetCashFlow = dfCashFlow.sum(axis=1) # Sorted yearly net cash flow
+    cumNetCashFlow = yearNetCashFlow.cumsum() # Cumulative net cash flow
+    labels = pd.DataFrame(dfCashFlow.columns) # Sorted labels by CompName and sum of values
 
     ## Plot 1: Inflows, outflows and Net Cash Flow
     widthFlowbar = 0.35
     _, ax1 = plt.subplots(1, figsize = (15,8))
-    for i, _ in enumerate(cumulativeCashFlow[0:negativeVariable]):
+    for i, _ in enumerate(cumCashFlow[0:negVar]):
       if i == 0:
-        ax1.bar(dfYear['cfYears'], dfCashFlow.iloc[:,i], widthFlowbar, color = cmapList[i], label = labels.iloc[i,0], edgecolor='white')
+        ax1.bar(dfYear['cfYears'], dfCashFlow.iloc[:,i], widthFlowbar, color = compColor[i],
+          label = labels.iloc[i,0], edgecolor='white')
       else:
-        ax1.bar(dfYear['cfYears'], dfCashFlow.iloc[:,i], widthFlowbar, color = cmapList[i], bottom = dfCashFlow.iloc[:,0:i-1].sum(axis=1), label = labels.iloc[i,0], edgecolor='white')
+        ax1.bar(dfYear['cfYears'], dfCashFlow.iloc[:,i], widthFlowbar, color = compColor[i],
+          bottom = dfCashFlow.iloc[:,0:i-1].sum(axis=1), label = labels.iloc[i,0], edgecolor='white')
 
-    for i, _ in enumerate(cumulativeCashFlow[negativeVariable:negativeVariable+positiveVariable]):
+    for i, _ in enumerate(cumCashFlow[negVar:negVar+posVar]):
       if i == 0:
-        ax1.bar(dfYear['cfYears'], dfCashFlow.iloc[:,i+negativeVariable], widthFlowbar, color = cmapList[i+negativeVariable], label = labels.iloc[i+negativeVariable,0], edgecolor='white')
+        ax1.bar(dfYear['cfYears'], dfCashFlow.iloc[:,i+negVar], widthFlowbar,
+          color = compColor[i+negVar], label = labels.iloc[i+negVar,0], edgecolor='white')
       else:
-        ax1.bar(dfYear['cfYears'], dfCashFlow.iloc[:,i+negativeVariable], widthFlowbar, color = cmapList[i+negativeVariable], bottom = dfCashFlow.iloc[:,negativeVariable:negativeVariable+i].sum(axis=1), label = labels.iloc[i+negativeVariable,0], edgecolor='white')
+        ax1.bar(dfYear['cfYears'], dfCashFlow.iloc[:,i+negVar], widthFlowbar,
+          color = compColor[i+negVar], bottom = dfCashFlow.iloc[:,negVar:negVar+i].sum(axis=1),
+          label = labels.iloc[i+negVar,0], edgecolor='white')
 
     # grid
     ax1.set_axisbelow(True)
@@ -144,66 +167,67 @@ class CashFlowPlot(PlotPlugin):
     box = ax1.get_position()
     ax1.set_position([box.x0, box.y0, box.width * 0.8, box.height])
     plt.legend(loc='center left', bbox_to_anchor=(1.0,0.5), fancybox=True, ncol=1,fontsize = 15)
-    plt.title('Yearly Inflows, Outflows and Net Cash Flow from Project Year {} to {}'.format(startYear, endYear), y=1.05, fontsize = 20, fontweight='bold')
+    plt.title('Yearly Inflows, Outflows and Net Cash Flow from Project Year {} to {}'.format(startYear, endYear),
+      y=1.05, fontsize = 20, fontweight='bold')
+
     fName = os.path.abspath('Cash_Flows_from_Project_Year_{}_to_{}.png'.format(startYear, endYear))
     plt.savefig(fName,bbox_inches="tight")
     self.raiseAMessage(f'Saved figure to "{fName}"')
 
     ## Plot 2: Cumulative discounted free cash flow
     lableLoc = np.arange(endYear + 1 - startYear)  # the label location
-    widthCumBar = 0.1
-    _, ax1 = plt.subplots(1, figsize = (22,12))
-    for i, _ in enumerate(cumulativeCashFlow):
-      ax1.bar(lableLoc + widthCumBar * (i - (len(cumulativeCashFlow) + 1) / 2), dfCashFlow.iloc[:,i], widthCumBar, color = cmapList[i], label = labels.iloc[i,0], edgecolor='white')
+    widthCumBar = 0.2
+    _, ax1 = plt.subplots(1, figsize = (25,12))
+    for i, _ in enumerate(cumCashFlow):
+      ax1.bar(lableLoc + widthCumBar * (i - (len(cumCashFlow) + 1) / 2), dfCashFlow.iloc[:,i],
+        widthCumBar, color = compColor[i], label = labels.iloc[i,0], edgecolor='white')
     #grid
     ax1.set_axisbelow(True)
     ax1.yaxis.grid(color='gray', linestyle='dashed', alpha=0.7)
     ax1.set_xlabel('Project Year', fontsize = 15, fontweight='bold')
     ax1.set_ylabel('Yearly Inflows and Outflows ($)', fontsize = 15, fontweight='bold')
-    # cumulative discounted free cash flow
+    # cumulative cash flow
     ax2 = ax1.twinx()
-    ax2.plot(dfYear['cfYears'], cumulativeNetCashFlow, color='darkolivegreen', marker='o',label='Cumulative Net Cash Flow')
+    ax2.plot(dfYear['cfYears'], cumNetCashFlow, color='darkolivegreen', marker='o',label='Cumulative Net Cash Flow')
     ax2.set_ylabel('Cumulative Net Cash Flow', color='darkolivegreen', fontsize = 15, fontweight='bold')
     ax2.tick_params(axis='y', labelcolor='darkolivegreen')
     # legends
-    ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.09),fancybox=True, ncol=negativeVariable+positiveVariable,fontsize = 15)
+    ax1.legend(loc='upper center', bbox_to_anchor=(0.5, -0.09),fancybox=True, ncol=negVar+posVar,fontsize = 15)
     ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),fancybox=True, ncol=1,fontsize = 15)
-    plt.title('Cumulative Discounted Free Cash Flow from Project Year {} to {}'.format(startYear, endYear), y=1.05, fontsize = 20, fontweight='bold')
+    plt.title('Cumulative Cash Flow from Project Year {} to {}'.format(startYear, endYear),
+      y=1.05, fontsize = 20, fontweight='bold')
     fName = os.path.abspath('Cumulative_from_Project_Year_{}_to_{}.png'.format(startYear, endYear))
     plt.savefig(fName,bbox_inches="tight")
     self.raiseAMessage(f'Saved figure to "{fName}"')
 
     ## Plot 3: pie chart with inflows and outflows
-    cumulativeCashFlowAbs = [abs(ele) for ele in cumulativeCashFlow]
-    cumOutFlow = cumulativeCashFlowAbs[0:negativeVariable] # donut for outflows when inflows are lower
-    cumInFlow = cumulativeCashFlowAbs[-positiveVariable:] # donut for inflows when outflows are lower
-    diff = sum(cumulativeCashFlow)
+    cumulativeCashFlowAbs = [abs(ele) for ele in cumCashFlow]
+    cumOutFlow = cumulativeCashFlowAbs[0:negVar] # donut for outflows when inflows are lower
+    cumInFlow = cumulativeCashFlowAbs[-posVar:] # donut for inflows when outflows are lower
+    diff = sum(cumCashFlow)
     cumIn = cumInFlow + [abs(diff)]  # donut for inflows when inflows are lower
     cumOut = cumOutFlow + [abs(diff)] # donut for outflows when outflows are lower
-    widthPieChart = 0.2
-    radiusPieChart = 0.8
+    widthPie = 0.2
+    radiusPie = 0.8
     _, ax1 = plt.subplots(1, figsize=[12,10])
     ax1.axis('equal')
     ax2 = ax1.twinx()
     if diff >= 0:
-      pieOut, _ = ax1.pie(cumOut, radius=radiusPieChart, colors = cmapList[0:negativeVariable]+[(0.0, 0.0, 0.0, 0.0)], startangle=90)
-      pieIn, _ = ax2.pie(cumInFlow, radius=radiusPieChart - widthPieChart, colors = cmapList[-positiveVariable:], startangle=90)
-      ax1.text(-0.55,0.86,"Inflows", color="darkblue", fontsize=16, fontweight='bold')
-      ax1.text(-0.29,0.86,"-              ≈", color="black", fontsize=18, fontweight='bold')
-      ax1.text(-0.25,0.86,"Outflows", color="darkred", fontsize=16, fontweight='bold')
-      ax1.text(0.14,0.86,f'${abs(diff): 1.3e}', color="royalblue", fontsize=16, fontweight='bold')
+      pieOut, _ = ax1.pie(cumOut, radius=radiusPie, colors = compColor[0:negVar]+[(0, 0, 0, 0)], startangle=90)
+      pieIn, _ = ax2.pie(cumInFlow, radius=radiusPie - widthPie, colors = compColor[-posVar:], startangle=90)
+      ax1.text(-0.55,0.86,"Outflows - Inflows = {}".format(f'${abs(diff): 1.3e}'),
+        color="black",fontsize=16, fontweight='bold')
     else:
-      pieOut, _ = ax1.pie(cumOutFlow, radius=radiusPieChart,colors = cmapList[0:negativeVariable], startangle=90)
-      pieIn, _ = ax2.pie(cumIn, radius=radiusPieChart - widthPieChart, colors = cmapList[-positiveVariable:]+[(0.0, 0.0, 0.0, 0.0)], startangle=90)
-      ax1.text(-0.55,0.86,"Outflows", color="darkred",fontsize=16, fontweight='bold')
-      ax1.text(-0.23,0.86,"-            ≈", color="black", fontsize=18, fontweight='bold')
-      ax1.text(-0.18,0.86,"Inflows", color="darkblue", fontsize=16, fontweight='bold')
-      ax1.text(0.16,0.86,f'${abs(diff): 1.3e}', color="salmon", fontsize=16, fontweight='bold')
+      pieOut, _ = ax1.pie(cumOutFlow, radius=radiusPie,colors = compColor[0:negVar], startangle=90)
+      pieIn, _ = ax2.pie(cumIn, radius=radiusPie-widthPie, colors = compColor[-posVar:]+[(0, 0, 0, 0)], startangle=90)
+      ax1.text(-0.55,0.86,"Outflows - Inflows = {}".format(f'${abs(diff): 1.3e}'),
+        color="black",fontsize=16, fontweight='bold')
 
-    ax1.legend(labels.iloc[0:negativeVariable,0], loc='lower right',fontsize = 15)
-    ax2.legend(labels.iloc[-positiveVariable:,0], loc='lower left',fontsize = 15)
-    plt.setp([pieOut, pieIn], width=widthPieChart, edgecolor='white')
-    plt.title('Cumulative Inflows vs. Outflows from Project Year {} to {}'.format(startYear, endYear), y=1, fontsize = 20, fontweight='bold')
+    ax1.legend(labels.iloc[0:negVar,0], loc='lower right',fontsize = 15)
+    ax2.legend(labels.iloc[-posVar:,0], loc='lower left',fontsize = 15)
+    plt.setp([pieOut, pieIn], width=widthPie, edgecolor='white')
+    plt.title('Cumulative Inflows vs. Outflows from Project Year {} to {}'.format(startYear, endYear),
+      y=1, fontsize = 20, fontweight='bold')
     fName = os.path.abspath('Cumulative_pie_from_Project_Year_{}_to_{}.png'.format(startYear, endYear))
     plt.savefig(fName)
     self.raiseAMessage(f'Saved figure to "{fName}"')
